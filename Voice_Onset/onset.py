8ac66896510de214eee4e935a320fa5bbb9f7414
Flo2306 <91254762+Flo2306@users.noreply.github.com>
2023-07-06 15:09:42 +0200

    Update onset.py

--- Voice_Onset/onset.py
+++ Voice_Onset/onset.py
@@ -81,6 +81,7 @@ def binary_search(audio_input, language_used, target_word, model = 'all-mpnet-ba
     #Creating a audio file that can be used for input. This file will be called "NEW_" plus the name of 
     #the original audio file but it will be removed once the code is finished
     current_directory_list = os.listdir()
+    print(current_directory_list)
     if audio_input in current_directory_list: 
         print("THANK FUCK")
     else: 
