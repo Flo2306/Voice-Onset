00cdb796bcab7d6a4c3a79ce1f54eeb8ace7dbd5
Flo2306 <91254762+Flo2306@users.noreply.github.com>
2023-08-13 16:41:26 +0200

    Update onset.py

--- Voice_Onset/onset.py
+++ Voice_Onset/onset.py
@@ -139,7 +139,7 @@ def binary_search(audio_input, language_used, target_word = None, model = 'all-m
         #Makes it slower but increases accuracy as it uses another method of transcribing audio
         
         if len(text) == 0: 
-            text = r.recognize_whisper(audio_data, language=language_used, show_all = True)
+            text = r.recognize_sphinx(audio_data, language=language_used, show_all = True)
         
         text_str = str(text)
 
