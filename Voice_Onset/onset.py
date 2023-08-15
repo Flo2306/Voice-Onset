3730851313dbf64d70ec570405787bc43a09f9f9
Flo2306 <91254762+Flo2306@users.noreply.github.com>
2023-08-15 11:25:26 +0200

    Update onset.py

--- Voice_Onset/onset.py
+++ Voice_Onset/onset.py
@@ -114,6 +114,7 @@ def binary_search(audio_input, language_used, target_word = None, model = 'all-m
         while True:
             try:
                 print("Fuck")
+                del audio_input
                 os.remove(audio_input)
                 break
             except PermissionError:
