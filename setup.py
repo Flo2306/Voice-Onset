ecdcfad9580b482d4f15952be01868c0a39933a0
Flo2306 <91254762+Flo2306@users.noreply.github.com>
2023-08-22 11:31:23 +0200

    Add Whisper to needed packages

--- setup.py
+++ setup.py
@@ -1,32 +1,35 @@
 from setuptools import setup
 
-setup(name='Voice_Onset',
-      version= "0.1",
-      description='A modul that can be used to detemine voice onset times',
-      url='https://github.com/Flo2306/Voice_Onset',
-      author='Florian Burger',
-      author_email='f.burger@uva.nl',
-      license='MIT',
-      packages=['Voice_Onset'],
-      package_data = {'Voice_Onset': ['Sample_Experiment/**']},
-      install_requires=[
+setup(
+    name='Voice_Onset',
+    version="0.1",
+    description='A module that can be used to determine voice onset times',
+    url='https://github.com/Flo2306/Voice_Onset',
+    author='Florian Burger',
+    author_email='f.burger@uva.nl',
+    license='MIT',
+    packages=['Voice_Onset'],
+    package_data={'Voice_Onset': ['Sample_Experiment/**']},
+    install_requires=[
         'SpeechRecognition',
         'pandas',
         'numpy',
-        'glob2', 
-        'soundfile',
+        'glob2',
         'scikit-learn',
         'sentence-transformers',
         'scipy',
         'unipath',
-        'numpy'
+        'numpy',
+        'git+https://github.com/openai/whisper.git',
+        'soundfile'
     ],
     classifiers=[
-        "Development Status :: 1 - Trial Setup",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
-        "Topic :: Scientific Research :: Voice Onset/Response Time"
+        "Topic :: Scientific/Engineering",
     ],
     keywords="Voice Onset Word Onset Voice Data Analysis Binary Search Word Similarity"
-    )
+)
+
